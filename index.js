d8238a83880efe84904576611f32f5646ecd57cf
frescocodes <hueydakotah@gmail.com>
2019-10-31 15:53:39 -0400

    chore: install dotenv, make port dynamic for heroku

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const server = express(); // create instance of express server
-const port = 8000
+const port = process.env.PORT || 8000;
 server.listen(port, () => console.log(`SERVER RUNNING ON PORT ${port}`))
 server.get('/', ( req , res ) => res.status(200).send({ message: "Server is running"}))
